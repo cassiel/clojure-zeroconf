4be9361b3f2433243ed7531a32adb055c694cc80
nick rothwell <nick@cassiel.com>
2013-10-17 13:13:18 +0100

    show-urls from AdamClements: bumped project version (and Clojure version).

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject eu.cassiel/clojure-zeroconf "1.1.0"
+(defproject eu.cassiel/clojure-zeroconf "1.2.0"
   :description "Zeroconf client in Clojure"
   :url "https://github.com/cassiel/clojure-zeroconf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[javax.jmdns/jmdns "3.4.1"]
-                 [org.clojure/clojure "1.5.0"]]
+                 [org.clojure/clojure "1.5.1"]]
   :plugins [[lein-marginalia "0.7.1"]])
